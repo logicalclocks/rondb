7de1d86a20495aeb6bfaa42a46bec8da6ac4b065
Varun Nagaraju <varun.nagaraju@oracle.com>
2021-08-11 18:42:34 +0530

    Bug#26974491 FIXES DATA RESTORE WITH DISABLE INDEXES
    Post-push fix to improve error reporting.
    
    Change-Id: I5d04e4c379994b6dd073daddabdf7257666300d1

--- storage/ndb/tools/restore/consumer_restore.cpp
+++ storage/ndb/tools/restore/consumer_restore.cpp
@@ -3500,6 +3500,7 @@ BackupRestore::endOfTables(){
                                  split_idx[3].c_str(), table_name.c_str(),
                                  dict->getNdbError().code,
                                  dict->getNdbError().message);
+          return false;
         }
       }
     }
