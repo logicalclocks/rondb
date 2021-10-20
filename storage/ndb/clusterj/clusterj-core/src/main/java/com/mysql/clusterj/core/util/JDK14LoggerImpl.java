3e08923214093ae5a363a0ee1c0f6d085f60c883
Mikael Ronström <mikael.ronstrom@gmail.com>
2021-10-20 15:48:53 +0200

    Fixed copyright headers and final merge 8.0.27

--- storage/ndb/clusterj/clusterj-core/src/main/java/com/mysql/clusterj/core/util/JDK14LoggerImpl.java
+++ storage/ndb/clusterj/clusterj-core/src/main/java/com/mysql/clusterj/core/util/JDK14LoggerImpl.java
@@ -1,8 +1,8 @@
 /*
    Copyright (c) 2010, 2021, Oracle and/or its affiliates.
+   Copyright (c) 2020, 2021 LogicalClocks and/or its affiliates.
    Use is subject to license terms.
 
-   Copyright (c) 2020, LogicalClocks AB and/or its affiliates.
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
    as published by the Free Software Foundation.
