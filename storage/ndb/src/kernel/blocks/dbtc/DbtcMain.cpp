8f2e7f5baf753c2f893b57cca820b6207fe21c21
Mikael Ronström <mikael.ronstrom@gmail.com>
2022-01-24 21:52:25 +0100

    RONDB-106: Test fixes

--- storage/ndb/src/kernel/blocks/dbtc/DbtcMain.cpp
+++ storage/ndb/src/kernel/blocks/dbtc/DbtcMain.cpp
@@ -4853,7 +4853,7 @@ void Dbtc::tckeyreq050Lab(Signal* signal,
     if (regTcPtr->tcNodedata[0] == getOwnNodeId())
       c_counters.clocalReadCount++;
 #ifdef ERROR_INSERT
-    else if (ERROR_INSERTED(8083))
+    else if (ERROR_INSERTED(8083) && !batchUnsafe)
     {
       ndbabort();  // Only node-local reads
     }
