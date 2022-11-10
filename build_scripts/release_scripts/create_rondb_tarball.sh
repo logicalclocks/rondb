58f40139e8a90d11748cd176b0ea32f1aa418ca1
Vincent Lohse <vincent.lohse@logicalclocks.com>
2022-11-10 19:41:12 +1100

    Removed dpkg dependency

--- build_scripts/release_scripts/create_rondb_tarball.sh
+++ build_scripts/release_scripts/create_rondb_tarball.sh
@@ -7,39 +7,43 @@ OUTPUT_DIR_ABS=$3
 
 cd $TEMP_BUILD_DIR_ABS
 
+# The following code of how to determine OS and CPU architecture is not optimal.
+# Essentially, we are trying to keep the naming conventions on repo.hops.works
+#   Examples:
+#   rondb-21.04.2-linux-glibc2.17-x86_64.tar.gz
+#   rondb-21.04.2-macos-xcode-13.1-arm64_v8.tar.gz
+
 # from https://stackoverflow.com/a/17072017/9068781
 if [ "$(uname)" == "Darwin" ]; then
   OS="macos"
   # from https://stackoverflow.com/a/42144725/9068781
   XCODE_VERSION=$(pkgutil --pkg-info=com.apple.pkg.CLTools_Executables | grep version | sed -n "s/version\: *\([[:digit:]]*\.[[:digit:]]*\).*$/\1/p")
-  COMPILER="xcode-${XCODE_VERSION}"
-  CPU_ARCH=$(uname -m)  # amd64 / arm64
+  STD_LIBRARY="xcode-${XCODE_VERSION}"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   OS="linux"
   GLIBC_VERSION=$(ldd --version | sed -n "s/.*Ubuntu GLIBC *\([[:digit:]]\.[[:digit:]][[:digit:]]\).*$/\1/p")
-  COMPILER="glibc${GLIBC_VERSION}"
-  CPU_ARCH=$(dpkg --print-architecture)  # amd64 / arm64
+  STD_LIBRARY="glibc${GLIBC_VERSION}"
 else
   echo "OS is neither Linux nor MacOS"
-  exit 1
+  STD_LIBRARY="unknown-os"
 fi
 
-# We are converting here to keep the naming convention on repo.hops.works
-#   Examples:
-#   rondb-21.04.2-linux-glibc2.17-x86_64.tar.gz
-#   rondb-21.04.2-macos-xcode-13.1-arm64_v8.tar.gz
-echo "WARNING: No solution figure out how to figure out OS variant"
-if [ "$CPU_ARCH" == "amd64" ]; then
+# x86_64    on Ubuntu, Oraclelinux7, Mac
+# aarch64   on Ubuntu, Oraclelinux7
+# amd64     on Mac
+CPU_ARCH=$(uname -m)
+
+if [ "$CPU_ARCH" == "amd64" -o "$CPU_ARCH" == "x86_64" ]; then
   CPU_ARCH_FULL="x86_64"
-elif [ "$CPU_ARCH" == "arm64" ]; then
-  echo "WARNING: Guessing that we are running with arm64_v8"
+elif [ "$CPU_ARCH" == "arm64" -o "$CPU_ARCH" == "aarch64" ]; then
+  # ARMv8 introduced 64-bit
   CPU_ARCH_FULL="arm64_v8"
 else
   echo "Cannot figure out the exact CPU architecture"
-  exit 1
+  CPU_ARCH_FULL="unknown_arch"
 fi
 
-TARBALL_NAME="rondb-${RONDB_VERSION}-${OS}-${COMPILER}-${CPU_ARCH_FULL}"
+TARBALL_NAME="rondb-${RONDB_VERSION}-${OS}-${STD_LIBRARY}-${CPU_ARCH_FULL}"
 
 rm -rf $TARBALL_NAME
 mv rondb_bin_use $TARBALL_NAME
