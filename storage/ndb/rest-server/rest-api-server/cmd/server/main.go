1e5df805570e97002cfe80da8361c6e55c6418c9
Vincent Lohse <vincent.lohse@logicalclocks.com>
2022-11-24 17:53:51 +1100

    Fixed help flag for -v

--- storage/ndb/rest-server/rest-api-server/cmd/server/main.go
+++ storage/ndb/rest-server/rest-api-server/cmd/server/main.go
@@ -37,7 +37,7 @@ import (
 
 func main() {
 	configFile := flag.String("config", "", "Configuration file path")
-	ver := flag.Bool("v", false, "Configuration file path")
+	ver := flag.Bool("v", false, "Print API and application version")
 	flag.Parse()
 
 	if *configFile != "" {
