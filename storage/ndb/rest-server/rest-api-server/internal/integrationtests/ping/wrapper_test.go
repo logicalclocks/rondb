b665d4f45c543d47b1d862ca8510c5dba3f2ce2a
Vincent Lohse <vincent.lohse@logicalclocks.com>
2023-04-06 10:09:47 +0000

    Removed runtime.GOMAXPROCS call in ping wrapper_test

--- storage/ndb/rest-server/rest-api-server/internal/integrationtests/ping/wrapper_test.go
+++ storage/ndb/rest-server/rest-api-server/internal/integrationtests/ping/wrapper_test.go
@@ -20,7 +20,6 @@ package ping
 import (
 	"fmt"
 	"os"
-	"runtime"
 	"runtime/debug"
 	"testing"
 
@@ -52,9 +51,6 @@ func TestMain(m *testing.M) {
 		log.Fatalf(err.Error())
 	}
 	defer cleanup()
-
-	// Less than NumCPUs seemed to work well here:
-	runtime.GOMAXPROCS(conf.Internal.GOMAXPROCS)
 
 	retcode = m.Run()
 }
