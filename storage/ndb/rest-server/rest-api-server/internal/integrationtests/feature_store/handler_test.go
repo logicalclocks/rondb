f7e2aec1b66f38b8c89495e65c2104b858a3d408
Salman Niazi <salman@logicalclocks.com>
2023-04-14 15:29:43 +0200

    Rondb 345 (#257)
    * RONDB-345: scaffolding code for feature store rest api
    
    * RONDB-345: removed heap from FS endpoint

---
+++ storage/ndb/rest-server/rest-api-server/internal/integrationtests/feature_store/handler_test.go
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the RonDB REST API Server
+ * Copyright (c) 2023 Hopsworks AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package feature_store
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"hopsworks.ai/rdrs/internal/config"
+	"hopsworks.ai/rdrs/internal/integrationtests/testclient"
+	"hopsworks.ai/rdrs/internal/log"
+	"hopsworks.ai/rdrs/internal/testutils"
+	"hopsworks.ai/rdrs/pkg/api"
+)
+
+func TestFeatureStore(t *testing.T) {
+	req := api.FeatureStoreRequest{ReqData: "data sent by client"}
+	reqBody := fmt.Sprintf("%s", req)
+
+	_, respBody := testclient.SendHttpRequest(t, config.FEATURE_STORE_HTTP_VERB, testutils.NewFeatureStoreURL(), reqBody, "", http.StatusOK)
+
+	fsResp := api.FeatureStoreResponse{}
+	err := json.Unmarshal([]byte(respBody), &fsResp)
+	if err != nil {
+		t.Fatalf("Unmarshal failed %s ", err)
+	}
+
+	log.Infof("Response data is %s", fsResp.RespData)
+}
