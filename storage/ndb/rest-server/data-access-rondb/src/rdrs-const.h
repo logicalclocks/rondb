logicalclocks/rondb

commit 3a89399f3c31cd7d274da6940135c67c53e107ac

RONDB-410: API Server. Fixed error handling in the C layer (#324)
* RONDB-410: API Server. Fixed error handling and memory leaks in the C layer

storage/ndb/rest-server/data-access-rondb/src/rdrs-const.h
@@ -24,6 +24,15 @@
 extern "C" {
 #endif
 
+// number of bytes need to store string length for varchar columns in NDB
+inline int bytes_for_ndb_str_len(int ndb_str_len) {
+  if (ndb_str_len <= 255) {
+    return 1;
+  } else {
+    return 2;
+  }
+}
+
 // 4 bytes. Max addressable memrory is 4GB
 // which is max supported blob size
 #define ADDRESS_SIZE 4
@@ -71,17 +80,23 @@ extern "C" {
 #define PK_RESP_HEADER_END     36  // NOTE: Update this when you add / remove fields from  header
 
 // Hopsworks
-#define API_KEY_PREFIX_SIZE                45 + 1
-#define PROJECT_TEAM_TEAM_MEMBER_SIZE      150 + 1
-#define PROJECT_PROJECTNAME_SIZE           100 + 1
-#define FEATURE_STORE_NAME_SIZE            100 + 1
-#define FEATURE_VIEW_NAME_SIZE             63 + 1
-#define TRAINING_DATASET_JOIN_PREFIX_SIZE  63 + 1
-#define FEATURE_GROUP_NAME_SIZE            63 + 1
-#define TRAINING_DATASET_FEATURE_NAME_SIZE 1000 + 2
-#define TRAINING_DATASET_FEATURE_TYPE_SIZE 1000 + 2
-#define ON_DEMAND_FEATURE_SIZE 1000 + 1
-#define CACHE_FEATURE_SIZE 63 + 1
+// Update the following constants if there are changes in the hopsworks schema
+#define API_KEY_PREFIX_SIZE                45 + 1   /* +1 for ndb len or '\0'*/
+#define API_KEY_NAME_SIZE                  45 + 1   /* +1 for ndb len or '\0'*/
+#define API_KEY_SECRET_SIZE                512 + 2  /* +2 for ndb len or '\0'*/
+#define API_KEY_SALT_SIZE                  256 + 2  /* +2 for ndb len or '\0'*/
+#define USERS_EMAIL_SIZE                   150 + 1  /* +1 for ndb len or '\0'*/
+#define PROJECT_TEAM_TEAM_MEMBER_SIZE      150 + 1  /* +1 for ndb len or '\0'*/
+#define PROJECT_PROJECTNAME_SIZE           100 + 1  /* +1 for ndb len or '\0'*/
+#define FEATURE_STORE_NAME_SIZE            100 + 1  /* +1 for ndb len or '\0'*/
+#define FEATURE_VIEW_NAME_SIZE             63 + 1   /* +1 for ndb len or '\0'*/
+#define TRAINING_DATASET_JOIN_PREFIX_SIZE  63 + 1   /* +1 for ndb len or '\0'*/
+#define FEATURE_GROUP_NAME_SIZE            63 + 1   /* +1 for ndb len or '\0'*/
+#define TRAINING_DATASET_FEATURE_NAME_SIZE 1000 + 2 /* +2 for ndb len or '\0'*/
+#define TRAINING_DATASET_FEATURE_TYPE_SIZE 1000 + 2 /* +2 for ndb len or '\0'*/
+#define TRAINING_DATASET_JOIN_PREFIX_SIZE  63 + 1   /* +1 for ndb len or '\0'*/
+#define ON_DEMAND_FEATURE_NAME_SIZE        1000 + 2 /* +2 for ndb len or '\0'*/
+#define CACHE_FEATURE_SIZE                 63 + 1   /* +1 for ndb len or '\0'*/
 
 // Data types
 #define DECIMAL_MAX_SIZE_IN_BYTES           9 * 4   /*4 bytes per 9 digits. 65/9 + 1 * 4*/

storage/ndb/rest-server/data-access-rondb/src/feature_store/feature_store.h
@@ -25,14 +25,15 @@ extern "C" {
 #define STORAGE_NDB_REST_SERVER_DATA_ACCESS_RONDB_SRC_FEATURE_STORE_FEATURE_STORE_H_
 
 #include "../rdrs-dal.h"
+#include "../rdrs-const.h"
 
 // some DS
 typedef struct Training_Dataset_Feature {
   int feature_id;
   int training_dataset;
   int feature_group_id;
-  char name[1000+1];
-  char data_type[1000+1] ;
+  char name[TRAINING_DATASET_FEATURE_NAME_SIZE];
+  char data_type[TRAINING_DATASET_FEATURE_TYPE_SIZE];
   int td_join_id;
   int idx;
   int label;
@@ -42,16 +43,19 @@ typedef struct Training_Dataset_Feature {
 
 typedef struct Training_Dataset_Join {
   int id;
-  char prefix[63+1];
+  char prefix[TRAINING_DATASET_JOIN_PREFIX_SIZE];
 } Training_Dataset_Join;
 
 typedef struct Feature_Group {
   int feature_store_id;
-  char name[1000+1];
+
+  // this is used to store the name of on_demand_feature or feature_view.
+  char name[FEATURE_VIEW_NAME_SIZE > ON_DEMAND_FEATURE_NAME_SIZE ? FEATURE_VIEW_NAME_SIZE
+                                                                 : ON_DEMAND_FEATURE_NAME_SIZE];
   int version;
   int online_enabled;
   int num_pk;
-  char** primary_key;
+  char **primary_key;
 } Feature_Group;
 
 /**
@@ -79,11 +83,13 @@ RS_Status find_feature_view_id(int feature_store_id, const char *feature_view_na
  * SELECT id AS td_join_id, feature_group AS feature_group_id, prefix FROM training_dataset_join
  * WHERE feature_view_id = {feature_view_id}
  */
-RS_Status find_training_dataset_join_data(int feature_view_id, Training_Dataset_Join **tdjs, int *tdjs_size);
+RS_Status find_training_dataset_join_data(int feature_view_id, Training_Dataset_Join **tdjs,
+                                          int *tdjs_size);
 
 /**
  * Find feature group data
- * SELECT name, online_enabled, feature_store_id, version FROM feature_group WHERE id = {feature_group_id}
+ * SELECT name, online_enabled, feature_store_id, version FROM feature_group WHERE id =
+ * {feature_group_id}
  */
 RS_Status find_feature_group_data(int feature_group_id, Feature_Group *fg);
 
@@ -97,7 +103,8 @@ RS_Status find_feature_store_data(int feature_store_id, char *name);
  * Find training_dataset_feature
  * SELECT * from training_dataset_feature  WHERE feature_view_id = {feature_view_id}
  */
-RS_Status find_training_dataset_data(int feature_view_id, Training_Dataset_Feature **tdf, int *tdf_size);
+RS_Status find_training_dataset_data(int feature_view_id, Training_Dataset_Feature **tdf,
+                                     int *tdf_size);
 
 #endif
 #ifdef __cplusplus

storage/ndb/rest-server/data-access-rondb/src/rdrs-dal.cpp
@@ -41,11 +41,7 @@
 RDRSRonDBConnection *rdrsRonDBConnection = nullptr;
 
 /**
- * Initialize NDB connection
- * @param connection_string NDB connection string {url}:{port}
- * @param find_available_node_ID if set to 1 then we will first find an available node id to
- * connect to
- * @return status
+ * Initialize RonDB connection
  */
 RS_Status init(const char *connection_string, unsigned int connection_pool_size,
                unsigned int *node_ids, unsigned int node_ids_len, unsigned int connection_retries,
@@ -84,7 +80,8 @@ RS_Status set_op_retry_props(const unsigned int retry_cont, const unsigned int r
 //--------------------------------------------------------------------------------------------------
 
 RS_Status shutdown_connection() {
-  return rdrsRonDBConnection->Shutdown();
+  delete rdrsRonDBConnection;
+  return RS_OK;
 }
 
 //--------------------------------------------------------------------------------------------------

storage/ndb/rest-server/data-access-rondb/src/rdrs-hopsworks-dal.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Hopsworks AB
+ * Copyright (C) 2022, 2023 Hopsworks AB
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -67,12 +67,14 @@ RS_Status find_api_key_int(Ndb *ndb_object, const char *prefix, HopsworksAPIKey 
   Uint32 col_size = (Uint32)table_dict->getColumn("prefix")->getSizeInBytes();
   assert(col_size == API_KEY_PREFIX_SIZE);
   size_t prefix_len = strlen(prefix);
-  if (prefix_len > col_size) {
+  if (prefix_len > (col_size - bytes_for_ndb_str_len(API_KEY_PREFIX_SIZE))) {
+    ndb_object->closeTransaction(tx);
     return RS_CLIENT_ERROR("Wrong length of the search key");
   }
 
+  // Note: api_key is varchar col.
   char cmp_str[API_KEY_PREFIX_SIZE];
-  memcpy(cmp_str + 1, prefix, prefix_len + 1); //+1 copy null terminator too
+  memcpy(cmp_str + bytes_for_ndb_str_len(API_KEY_PREFIX_SIZE), prefix, prefix_len);
   cmp_str[0] = static_cast<char>(prefix_len);
 
   NdbScanFilter filter(scanOp);
@@ -89,12 +91,18 @@ RS_Status find_api_key_int(Ndb *ndb_object, const char *prefix, HopsworksAPIKey 
   NdbRecAttr *salt    = scanOp->getValue("salt");
   NdbRecAttr *name    = scanOp->getValue("name");
 
+  assert(API_KEY_SECRET_SIZE == (Uint32)table_dict->getColumn("secret")->getSizeInBytes());
+  assert(API_KEY_SALT_SIZE == (Uint32)table_dict->getColumn("salt")->getSizeInBytes());
+  assert(API_KEY_NAME_SIZE == (Uint32)table_dict->getColumn("name")->getSizeInBytes());
+
   if (user_id == nullptr || secret == nullptr || salt == nullptr || name == nullptr) {
+    err = scanOp->getNdbError();
+    ndb_object->closeTransaction(tx);
     return RS_RONDB_SERVER_ERROR(err, ERROR_019);
   }
 
   if (tx->execute(NdbTransaction::NoCommit) != 0) {
-    err = ndb_object->getNdbError();
+    err = tx->getNdbError();
     ndb_object->closeTransaction(tx);
     return RS_RONDB_SERVER_ERROR(err, ERROR_009);
   }
@@ -113,34 +121,40 @@ RS_Status find_api_key_int(Ndb *ndb_object, const char *prefix, HopsworksAPIKey 
       Uint32 name_attr_bytes;
       const char *name_data_start = nullptr;
       if (GetByteArray(name, &name_data_start, &name_attr_bytes) != 0) {
+        ndb_object->closeTransaction(tx);
         return RS_CLIENT_ERROR(ERROR_019);
       }
 
       Uint32 salt_attr_bytes;
       const char *salt_data_start = nullptr;
       if (GetByteArray(salt, &salt_data_start, &salt_attr_bytes) != 0) {
+        ndb_object->closeTransaction(tx);
         return RS_CLIENT_ERROR(ERROR_019);
       }
 
       Uint32 secret_attr_bytes;
       const char *secret_data_start = nullptr;
       if (GetByteArray(secret, &secret_data_start, &secret_attr_bytes) != 0) {
+        ndb_object->closeTransaction(tx);
         return RS_CLIENT_ERROR(ERROR_019);
       }
 
-      if (sizeof(api_key->secret) < secret_attr_bytes || sizeof(api_key->name) < name_attr_bytes ||
-          sizeof(api_key->salt) < salt_attr_bytes) {
+      // <= because we want to leave one byte for '\0' 
+      // sizes of char arrays are set to accommodate additional '\0'
+      if (sizeof(api_key->secret) <= secret_attr_bytes || sizeof(api_key->name) <= name_attr_bytes ||
+          sizeof(api_key->salt) <= salt_attr_bytes) {
+        ndb_object->closeTransaction(tx);
         return RS_CLIENT_ERROR(ERROR_021);
       }
 
       memcpy(api_key->name, name_data_start, name_attr_bytes);
-      api_key->name[name_attr_bytes] = 0;
+      api_key->name[name_attr_bytes] = '\0';
 
       memcpy(api_key->secret, secret_data_start, secret_attr_bytes);
-      api_key->secret[secret_attr_bytes] = 0;
+      api_key->secret[secret_attr_bytes] = '\0';
 
       memcpy(api_key->salt, salt_data_start, salt_attr_bytes);
-      api_key->salt[salt_attr_bytes] = 0;
+      api_key->salt[salt_attr_bytes] = '\0';
 
       api_key->user_id = user_id->int32_value();
     } while ((check = scanOp->nextResult(false)) == 0);
@@ -222,13 +236,13 @@ RS_Status find_user_int(Ndb *ndb_object, Uint32 uid, HopsworksUsers *users) {
   }
 
   NdbRecAttr *email = scanOp->getValue("email");
-
   if (email == nullptr) {
     return RS_RONDB_SERVER_ERROR(err, ERROR_019);
   }
+  assert(USERS_EMAIL_SIZE == (Uint32)table_dict->getColumn("email")->getSizeInBytes());
 
   if (tx->execute(NdbTransaction::NoCommit) != 0) {
-    err = ndb_object->getNdbError();
+    err = tx->getNdbError();
     ndb_object->closeTransaction(tx);
     return RS_RONDB_SERVER_ERROR(err, ERROR_009);
   }
@@ -239,10 +253,12 @@ RS_Status find_user_int(Ndb *ndb_object, Uint32 uid, HopsworksUsers *users) {
       Uint32 email_attr_bytes;
       const char *email_data_start = nullptr;
       if (GetByteArray(email, &email_data_start, &email_attr_bytes) != 0) {
+        ndb_object->closeTransaction(tx);
         return RS_CLIENT_ERROR(ERROR_019);
       }
 
       if (sizeof(users->email) < email_attr_bytes) {
+        ndb_object->closeTransaction(tx);
         return RS_CLIENT_ERROR(ERROR_021);
       }
 
@@ -273,6 +289,7 @@ RS_Status find_user(Uint32 uid, HopsworksUsers *users) {
   }
 
   status = find_user_int(ndb_object, uid, users);
+
   rdrsRonDBConnection->ReturnNDBObjectToPool(ndb_object, &status);
 
   return status;
@@ -311,13 +328,16 @@ RS_Status find_project_team_int(Ndb *ndb_object, HopsworksUsers *users,
   int col_id      = table_dict->getColumn("team_member")->getColumnNo();
   Uint32 col_size = (Uint32)table_dict->getColumn("team_member")->getSizeInBytes();
   assert(col_size == PROJECT_TEAM_TEAM_MEMBER_SIZE);
+
   size_t email_len = strlen(users->email);
-  if (email_len > col_size) {
+  if (email_len > (col_size - bytes_for_ndb_str_len(PROJECT_TEAM_TEAM_MEMBER_SIZE))) {
+    ndb_object->closeTransaction(tx);
     return RS_CLIENT_ERROR("Wrong length of the search key");
   }
 
+  // Note: project_team is varchar col.
   char cmp_str[PROJECT_TEAM_TEAM_MEMBER_SIZE];
-  memcpy(cmp_str + 1, users->email, email_len + 1);  //+1 for null terminator
+  memcpy(cmp_str + bytes_for_ndb_str_len(PROJECT_TEAM_TEAM_MEMBER_SIZE), users->email, email_len);
   cmp_str[0] = static_cast<char>(email_len);
 
   NdbScanFilter filter(scanOp);
@@ -332,11 +352,12 @@ RS_Status find_project_team_int(Ndb *ndb_object, HopsworksUsers *users,
   NdbRecAttr *project_id = scanOp->getValue("project_id");
 
   if (project_id == nullptr) {
+    ndb_object->closeTransaction(tx);
     return RS_RONDB_SERVER_ERROR(err, ERROR_019);
   }
 
   if (tx->execute(NdbTransaction::NoCommit) != 0) {
-    err = ndb_object->getNdbError();
+    err = tx->getNdbError();
     ndb_object->closeTransaction(tx);
     return RS_RONDB_SERVER_ERROR(err, ERROR_009);
   }
@@ -373,6 +394,7 @@ RS_Status find_project_team(HopsworksUsers *users,
   }
 
   status = find_project_team_int(ndb_object, users, project_team_vec);
+  
   rdrsRonDBConnection->ReturnNDBObjectToPool(ndb_object, &status);
 
   return status;
@@ -436,11 +458,13 @@ RS_Status find_projects_int(Ndb *ndb_object, std::vector<HopsworksProjectTeam> *
   NdbRecAttr *projectname = scanOp->getValue("projectname");
 
   if (projectname == nullptr) {
+    ndb_object->closeTransaction(tx);
     return RS_RONDB_SERVER_ERROR(err, ERROR_019);
   }
+  assert(PROJECT_PROJECTNAME_SIZE == (Uint32)table_dict->getColumn("projectname")->getSizeInBytes());
 
   if (tx->execute(NdbTransaction::NoCommit) != 0) {
-    err = ndb_object->getNdbError();
+    err = tx->getNdbError();
     ndb_object->closeTransaction(tx);
     return RS_RONDB_SERVER_ERROR(err, ERROR_009);
   }
@@ -452,15 +476,17 @@ RS_Status find_projects_int(Ndb *ndb_object, std::vector<HopsworksProjectTeam> *
       Uint32 projectname_attr_bytes;
       const char *projectname_data_start = nullptr;
       if (GetByteArray(projectname, &projectname_data_start, &projectname_attr_bytes) != 0) {
+        ndb_object->closeTransaction(tx);
         return RS_CLIENT_ERROR(ERROR_019);
       }
 
-      if (sizeof(project.porjectname) < projectname_attr_bytes) {
+      if (sizeof(project.projectname) < projectname_attr_bytes) {
+        ndb_object->closeTransaction(tx);
         return RS_CLIENT_ERROR(ERROR_021);
       }
 
-      memcpy(project.porjectname, projectname_data_start, projectname_attr_bytes);
-      project.porjectname[projectname_attr_bytes] = 0;
+      memcpy(project.projectname, projectname_data_start, projectname_attr_bytes);
+      project.projectname[projectname_attr_bytes] = '\0';
       project_vec->push_back(project);
 
     } while ((check = scanOp->nextResult(false)) == 0);
@@ -489,6 +515,7 @@ RS_Status find_projects_vec(std::vector<HopsworksProjectTeam> *project_team_vec,
   }
 
   status = find_projects_int(ndb_object, project_team_vec, project_vec);
+
   rdrsRonDBConnection->ReturnNDBObjectToPool(ndb_object, &status);
 
   return status;
@@ -536,8 +563,8 @@ RS_Status find_all_projects(int uid, char ***projects, int *count) {
 
   char **ease = *projects;
   for (Uint32 i = 0; i < project_vec.size(); i++) {
-    ease[i] = (char *)malloc(sizeof(dummy.porjectname) * sizeof(char));  // freed by CGO
-    memcpy(ease[i], project_vec[i].porjectname, strlen(project_vec[i].porjectname) + 1);
+    ease[i] = (char *)malloc(sizeof(dummy.projectname) * sizeof(char));  // freed by CGO
+    memcpy(ease[i], project_vec[i].projectname, strlen(project_vec[i].projectname) + 1);
   }
   return RS_OK;
 }

storage/ndb/rest-server/data-access-rondb/src/rdrs-hopsworks-dal.h
@@ -25,18 +25,19 @@ extern "C" {
 #define STORAGE_NDB_REST_SERVER_DATA_ACCESS_RONDB_SRC_RDRS_HOPSWORKS_DAL_H_
 
 #include "rdrs-dal.h"
+#include "rdrs-const.h"
 
 // API Key table
 typedef struct HopsworksAPIKey {
-  char secret[513];
-  char salt[257];
-  char name[46];
+  char secret[API_KEY_SECRET_SIZE];
+  char salt[API_KEY_SALT_SIZE];
+  char name[API_KEY_NAME_SIZE];
   int user_id;
 } HopsworksAPIKey;
 
 // User table
 typedef struct HopsworksUsers {
-  char email[151];
+  char email[USERS_EMAIL_SIZE];
 } HopsworksUsers;
 
 // project_team table
@@ -46,7 +47,7 @@ typedef struct HopsworksProjectTeam {
 
 // project_team table
 typedef struct HopsworksProject {
-  char porjectname[101];
+  char projectname[PROJECT_PROJECTNAME_SIZE];
 } HopsworksProject;
 
 /**

storage/ndb/rest-server/data-access-rondb/src/rdrs_rondb_connection.cpp
@@ -55,7 +55,7 @@ RS_Status RDRSRonDBConnection::Init(const char *connection_string, Uint32 connec
   size_t connection_string_len  = strlen(connection_string);
   __instance->connection_string = reinterpret_cast<char *>(malloc(connection_string_len + 1));
   std::strncpy(__instance->connection_string, connection_string, connection_string_len);
-  __instance->connection_string[connection_string_len] = 0;
+  __instance->connection_string[connection_string_len] = '\0';
   __instance->connection_pool_size                     = connection_pool_size;
 
   __instance->node_ids = reinterpret_cast<Uint32 *>(malloc(node_ids_len * sizeof(Uint32)));
@@ -119,6 +119,11 @@ RS_Status RDRSRonDBConnection::Connect() {
 
   LOG_INFO("RonDB connection and object pool initialized");
   return RS_OK;
+}
+
+//--------------------------------------------------------------------------------------------------
+RDRSRonDBConnection::~RDRSRonDBConnection() {
+  Shutdown(false);
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -205,7 +210,7 @@ void RDRSRonDBConnection::ReturnNDBObjectToPool(Ndb *ndb_object, RS_Status *stat
     availableNdbObjects.push_back(ndb_object);
   }
 
-  // Note there are not unit test for this
+  // Note there are no unit test for this
   // Inorder to test this run the  TestReconnection1 for longer duration
   // and then drop the ndbconnection  using iptables or by disconnection the network
   if (status != nullptr && status->http_code != SUCCESS) {  // check for errors
@@ -228,13 +233,43 @@ RonDB_Stats RDRSRonDBConnection::GetStats() {
 }
 
 //--------------------------------------------------------------------------------------------------
-RS_Status RDRSRonDBConnection::Shutdown() {
-  return Shutdown(false);
-}
-
-//--------------------------------------------------------------------------------------------------
 
 RS_Status RDRSRonDBConnection::Shutdown(bool end) {
+
+  // wait for all NDB objects to return
+  using namespace std::chrono;
+  Int64 startTime   = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+  Int64 timeElapsed = 0;
+
+  bool allNDBObjectsCountedFor = false;
+  do {
+
+    size_t expectedSize = 0;
+    Uint32 sizeGot      = 0;
+    {
+      std::lock_guard<std::mutex> guard(connectionMutex);
+      sizeGot      = availableNdbObjects.size();
+      expectedSize = stats.ndb_objects_created;
+    }
+
+    if (expectedSize != sizeGot) {
+      LOG_WARN("Waiting to all NDB objects to return before shutdown. Expected Size: " +
+               std::to_string(expectedSize) + " Have: " + std::to_string(sizeGot));
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    } else {
+      allNDBObjectsCountedFor = true;
+      break;
+    }
+    timeElapsed =
+        duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - startTime;
+  } while (timeElapsed < 120 * 1000);
+
+  if (!allNDBObjectsCountedFor) {
+    LOG_ERROR("Timedout waiting for all NDB objects.");
+  } else {
+    LOG_INFO("All NDB objects are accounted for. Total objects: " +
+             std::to_string(stats.ndb_objects_created));
+  }
 
   LOG_DEBUG("Shutting down RonDB connection and NDB object pool");
 
@@ -280,8 +315,8 @@ RS_Status RDRSRonDBConnection::Shutdown(bool end) {
     if (end) {
       stats.is_shutdown = true;
       ndb_end(1);  // sometimes causes seg faults when called repeatedly from unit tests
-      delete connection_string;
-      delete node_ids;
+      free(connection_string);
+      free(node_ids);
       if (reconnectionThread != nullptr) {
         NdbThread_Destroy(&reconnectionThread);
         reconnectionThread = nullptr;
@@ -299,41 +334,6 @@ RS_Status RDRSRonDBConnection::ReconnectHandler() {
   {
     std::lock_guard<std::mutex> guardInfo(connectionInfoMutex);
     require(stats.is_reconnection_in_progress);
-  }
-
-  // stop all the NDB objects
-  using namespace std::chrono;
-  Int64 startTime   = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-  Int64 timeElapsed = 0;
-
-  bool allNDBObjectsCountedFor = false;
-  do {
-
-    size_t expectedSize = 0;
-    Uint32 sizeGot      = 0;
-    {
-      std::lock_guard<std::mutex> guard(connectionMutex);
-      sizeGot      = availableNdbObjects.size();
-      expectedSize = stats.ndb_objects_created;
-    }
-
-    if (expectedSize != sizeGot) {
-      LOG_WARN("Waiting to all NDB objects to return before shutdown. Expected Size: " +
-               std::to_string(expectedSize) + " Have: " + std::to_string(sizeGot));
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    } else {
-      allNDBObjectsCountedFor = true;
-      break;
-    }
-    timeElapsed =
-        duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - startTime;
-  } while (timeElapsed < 120 * 1000);
-
-  if (!allNDBObjectsCountedFor) {
-    LOG_ERROR("Timedout waiting for all NDB objects.");
-  } else {
-    LOG_INFO("All NDB objects are accounted for. Total objects: " +
-             std::to_string(stats.ndb_objects_created));
   }
 
   RS_Status status = Shutdown(false);

storage/ndb/rest-server/data-access-rondb/src/rdrs_rondb_connection.hpp
@@ -63,6 +63,9 @@ class RDRSRonDBConnection {
   }
 
  public:
+
+  ~RDRSRonDBConnection();
+
   /**
    * Static method for initializing connection and NDB Object pool
    *
@@ -89,10 +92,10 @@ class RDRSRonDBConnection {
   /**
    * Returns Ndb object
    *
-   * New resource will be created if all the resources
-   * were used at the time of the request.
+   * New NDB object will be created if all 
+   * existing NDB Objects are in use
    *
-   * @return Status and Resource instance.
+   * @return Status and NDB object  
    */
   RS_Status GetNdbObject(Ndb **ndb_object);
 
@@ -111,12 +114,6 @@ class RDRSRonDBConnection {
    * @return RonDB_Stats
    */
   RonDB_Stats GetStats();
-
-  /**
-   * Purge. Delete all Ndb objects
-   *
-   */
-  RS_Status Shutdown();
 
   /**
    * Starts reconnection therad which calls the ReconnectHandler

commit d6ecfcfbaae468346850636ffe8c22b4336a10d2

RONDB-410: Fix linker problem
Fix a linker failure introduced in commit 3a89399f3c3

storage/ndb/rest-server/data-access-rondb/src/rdrs-const.h
@@ -25,7 +25,7 @@ extern "C" {
 #endif
 
 // number of bytes need to store string length for varchar columns in NDB
-inline int bytes_for_ndb_str_len(int ndb_str_len) {
+static inline int bytes_for_ndb_str_len(int ndb_str_len) {
   if (ndb_str_len <= 255) {
     return 1;
   } else {