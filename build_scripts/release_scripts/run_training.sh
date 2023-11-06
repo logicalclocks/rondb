ae023b4b17965a4757dce46ebd745cfb7b94df0c
Salman Niazi <salman@logicalclocks.com>
2023-11-06 09:52:53 +0100

    RonDB-530,  RONDB-529 Fixed Clusterj Test Suite

--- build_scripts/release_scripts/run_training.sh
+++ build_scripts/release_scripts/run_training.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-./mtr --suite=ndb ndb_basic ndb_dd_basic
+./mtr --suite=ndb ndb_basic ndb_dd_basic clusterj
 ./mtr --suite=ndb_opt
 ./mtr --suite=ndbcluster
 ./mtr --suite=ndb_rpl ndb_rpl_basic
 ./mtr --suite=innodb innodb-index
+./mtr --suite=rdrs 
