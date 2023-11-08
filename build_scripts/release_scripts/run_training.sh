f244079484bb1f1d4aa12064d832c6b2fe50ac0b
Salman Niazi <salman@logicalclocks.com>
2023-11-08 09:40:09 +0100

    RONDB-540: Docker build fixes for 22.10 (#401)

--- build_scripts/release_scripts/run_training.sh
+++ build_scripts/release_scripts/run_training.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+
+mysql_connector=mysql-connector-java-8.0.21-bin.jar
+wget https://repo.hops.works/master/$mysql_connector
+full_path=$(readlink -f $mysql_connector)
+export MTR_CLASSPATH=$full_path
+
 ./mtr --suite=ndb ndb_basic ndb_dd_basic clusterj
 ./mtr --suite=ndb_opt
 ./mtr --suite=ndbcluster
