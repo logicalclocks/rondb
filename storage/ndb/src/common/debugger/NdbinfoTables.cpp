8461d01cd1975baab098396ac2c0039dc3f03637
Mikael Ronström <mikael.ronstrom@gmail.com>
2024-07-13 12:13:15 +0200

    RONDB-624:  Fixed a final merge issue in ndbinfo_plans and final merge of test cases for full merge step

--- storage/ndb/src/common/debugger/NdbinfoTables.cpp
+++ storage/ndb/src/common/debugger/NdbinfoTables.cpp
@@ -159,7 +159,7 @@ DECLARE_NDBINFO_TABLE(LOGBUFFERS, 7) = {
 
 DECLARE_NDBINFO_TABLE(RESOURCES, 7) = {
     {"resources", 7, 0,
-     [](const Ndbinfo::Counts &c) { return c.data_nodes * 9 /*MM_RG_COUNT*/; },
+     [](const Ndbinfo::Counts &c) { return c.data_nodes * 13 /*MM_RG_COUNT*/; },
      "resources usage (a.k.a superpool)"},
     {{"node_id", Ndbinfo::Number, ""},
      {"resource_id", Ndbinfo::Number, ""},
@@ -835,7 +835,7 @@ DECLARE_NDBINFO_TABLE(TABLE_FRAGMENTS, 15) = {
 DECLARE_NDBINFO_TABLE(TABLE_REPLICAS, 16) = {
     {"table_replicas", 16, 0,
      [](const Ndbinfo::Counts &c) {
-       return c.data_nodes * 2 * c.est_tables;
+       return c.data_nodes * 4 * c.est_tables;
      },
      "Fragment replicas of the tables"},
     {{"node_id", Ndbinfo::Number, "node_id"},
@@ -906,7 +906,7 @@ DECLARE_NDBINFO_TABLE(TABLE_FRAGMENTS_ALL, 15) = {
 DECLARE_NDBINFO_TABLE(TABLE_REPLICAS_ALL, 16) = {
     {"table_replicas_all", 16, 0,
      [](const Ndbinfo::Counts &c) {
-       return c.instances.lqh * 2 * c.est_tables;
+       return 2 * c.est_tables * c.data_nodes;
      },
      "Fragment replicas of the tables"},
     {{"node_id", Ndbinfo::Number, "node_id"},
