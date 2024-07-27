06b93d0208d72bdde33b9902f11bf97640661b28
Mikael Ronström <mikael.ronstrom@gmail.com>
2024-07-27 03:28:16 +0200

    Fix for compilation using LLVM17 backported from FreeBSD fix of MySQL

--- sql/auth/sql_authorization.cc
+++ sql/auth/sql_authorization.cc
@@ -7430,6 +7430,10 @@ bool operator==(const Role_id &a, const std::string &b) {
   return tmp == b;
 }
 
+bool operator==(const Role_id &a, const LEX_CSTRING &b) {
+  return a == to_string(b);
+}
+
 bool operator==(const std::pair<Role_id, bool> &a, const std::string &b) {
   return a.first == b;
 }
