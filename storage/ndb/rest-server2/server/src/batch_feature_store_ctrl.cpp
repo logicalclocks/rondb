12d96fa9c5189ddc455bcb06db31b0d70d85e8a5
Mikael Ronström <mikael.ronstrom@gmail.com>
2024-09-27 12:08:42 +0200

    RONDB-736: Improve formatting of batch_feature_store_ctrl.cpp

--- storage/ndb/rest-server2/server/src/batch_feature_store_ctrl.cpp
+++ storage/ndb/rest-server2/server/src/batch_feature_store_ctrl.cpp
@@ -67,10 +67,12 @@ void BatchFeatureStoreCtrl::batch_featureStore(
   feature_store_data_structs::BatchFeatureStoreRequest reqStruct;
   auto status = jsonParser.batch_feature_store_parse(
       simdjson::padded_string_view(jsonParser.get_buffer().get(), length,
-                                   globalConfigs.internal.reqBufferSize + simdjson::SIMDJSON_PADDING),
+                                   globalConfigs.internal.reqBufferSize +
+                                   simdjson::SIMDJSON_PADDING),
       reqStruct);
 
-  if (static_cast<drogon::HttpStatusCode>(status.http_code) != drogon::HttpStatusCode::k200OK) {
+  if (static_cast<drogon::HttpStatusCode>(status.http_code) !=
+        drogon::HttpStatusCode::k200OK) {
     resp->setBody(std::string(std::string("Error:") + status.message));
     resp->setStatusCode(drogon::HttpStatusCode::k400BadRequest);
     callback(resp);
@@ -83,7 +85,9 @@ void BatchFeatureStoreCtrl::batch_featureStore(
   // Complete validation is delegated to Execute()
   // check if requested fv and fs exist
   auto [metadata, err] = batch_fvMetaCache.Get(
-      reqStruct.featureStoreName, reqStruct.featureViewName, reqStruct.featureViewVersion);
+      reqStruct.featureStoreName,
+      reqStruct.featureViewName,
+      reqStruct.featureViewVersion);
   if (err != nullptr) {
     resp->setBody(err->Error());
     resp->setStatusCode(drogon::HttpStatusCode::k400BadRequest);
@@ -100,9 +104,9 @@ void BatchFeatureStoreCtrl::batch_featureStore(
 
   if (!reqStruct.passedFeatures.empty() &&
       reqStruct.entries.size() != reqStruct.passedFeatures.size()) {
-    resp->setBody(INCORRECT_PASSED_FEATURE
-                      ->NewMessage("Length of passed feature does not equal to that of the entries "
-                                   "provided in the request.")
+    resp->setBody(INCORRECT_PASSED_FEATURE->NewMessage(
+      "Length of passed feature does not equal to that of the entries "
+      "provided in the request.")
                       ->GetMessage());
     resp->setStatusCode(drogon::HttpStatusCode::k400BadRequest);
     callback(resp);
@@ -120,7 +124,8 @@ void BatchFeatureStoreCtrl::batch_featureStore(
     }
     // Validate access right to ALL feature stores including shared feature
     auto status = authenticate(api_key, metadata->featureStoreNames);
-    if (static_cast<drogon::HttpStatusCode>(status.http_code) != drogon::HttpStatusCode::k200OK) {
+    if (static_cast<drogon::HttpStatusCode>(status.http_code) !=
+          drogon::HttpStatusCode::k200OK) {
       resp->setBody(std::string(status.message));
       resp->setStatusCode(drogon::HttpStatusCode::k401Unauthorized);
       callback(resp);
@@ -130,15 +135,20 @@ void BatchFeatureStoreCtrl::batch_featureStore(
 
   // Execute
   auto featureStatus =
-      std::vector<feature_store_data_structs::FeatureStatus>(reqStruct.entries.size());
-  auto detailedStatus = std::vector<std::vector<feature_store_data_structs::DetailedStatus>>();
+      std::vector<feature_store_data_structs::FeatureStatus>(
+        reqStruct.entries.size());
+  auto detailedStatus =
+    std::vector<std::vector<feature_store_data_structs::DetailedStatus>>();
   if (reqStruct.GetOptions().includeDetailedStatus) {
-    detailedStatus = std::vector<std::vector<feature_store_data_structs::DetailedStatus>>(
+    detailedStatus =
+      std::vector<std::vector<feature_store_data_structs::DetailedStatus>>(
         reqStruct.entries.size());
   }
   auto numPassed = checkFeatureStatus(reqStruct, *metadata, featureStatus);
   auto readParams =
-      getBatchPkReadParamsMultipleEntries(*metadata, reqStruct.entries, featureStatus);
+    getBatchPkReadParamsMultipleEntries(*metadata,
+                                        reqStruct.entries,
+                                        featureStatus);
   auto fsResp = feature_store_data_structs::BatchFeatureStoreResponse();
 
   auto features = std::vector<std::vector<std::vector<char>>>();
@@ -147,9 +157,11 @@ void BatchFeatureStoreCtrl::batch_featureStore(
     // Validate batch pkread
     for (auto readParam : readParams) {
       status = readParam.validate();
-      if (static_cast<drogon::HttpStatusCode>(status.http_code) != drogon::HttpStatusCode::k200OK) {
+      if (static_cast<drogon::HttpStatusCode>(status.http_code) !=
+            drogon::HttpStatusCode::k200OK) {
         resp->setBody(
-            TranslateRonDbError(drogon::HttpStatusCode::k400BadRequest, status.message)->Error());
+            TranslateRonDbError(drogon::HttpStatusCode::k400BadRequest,
+                                status.message)->Error());
         resp->setStatusCode(drogon::HttpStatusCode::k400BadRequest);
         callback(resp);
         return;
@@ -161,7 +173,6 @@ void BatchFeatureStoreCtrl::batch_featureStore(
     // Execute batch pkread
     std::vector<RS_Buffer> reqBuffs(noOps);
     std::vector<RS_Buffer> respBuffs(noOps);
-
     for (unsigned long i = 0; i < noOps; i++) {
       RS_Buffer reqBuff  = rsBufferArrayManager.get_req_buffer();
       RS_Buffer respBuff = rsBufferArrayManager.get_resp_buffer();
@@ -169,47 +180,52 @@ void BatchFeatureStoreCtrl::batch_featureStore(
       reqBuffs[i]  = reqBuff;
       respBuffs[i] = respBuff;
 
-      status = create_native_request(readParams[i], reqBuff.buffer, respBuff.buffer);
-      if (static_cast<drogon::HttpStatusCode>(status.http_code) != drogon::HttpStatusCode::k200OK) {
+      status = create_native_request(readParams[i],
+                                     reqBuff.buffer,
+                                     respBuff.buffer);
+      if (static_cast<drogon::HttpStatusCode>(status.http_code) !=
+            drogon::HttpStatusCode::k200OK) {
         resp->setBody(std::string(status.message));
         resp->setStatusCode(drogon::HttpStatusCode::k400BadRequest);
         callback(resp);
         return;
       }
-      uintptr_t length_ptr = reinterpret_cast<uintptr_t>(reqBuff.buffer) +
-                             static_cast<uintptr_t>(PK_REQ_LENGTH_IDX) * ADDRESS_SIZE;
+      uintptr_t length_ptr =
+        reinterpret_cast<uintptr_t>(reqBuff.buffer) +
+          static_cast<uintptr_t>(PK_REQ_LENGTH_IDX) * ADDRESS_SIZE;
       uint32_t *length_ptr_casted = reinterpret_cast<uint32_t *>(length_ptr);
-      reqBuffs[i].size            = *length_ptr_casted;
+      reqBuffs[i].size = *length_ptr_casted;
     }
-
     // pk_batch_read
     status = pk_batch_read(noOps, reqBuffs.data(), respBuffs.data());
-    if (static_cast<drogon::HttpStatusCode>(status.http_code) != drogon::HttpStatusCode::k200OK) {
+    if (static_cast<drogon::HttpStatusCode>(status.http_code) !=
+          drogon::HttpStatusCode::k200OK) {
       auto fsError = TranslateRonDbError(status.http_code, status.message);
       resp->setBody(fsError->Error());
-      resp->setStatusCode(static_cast<drogon::HttpStatusCode>(fsError->GetStatus()));
+      resp->setStatusCode(
+        static_cast<drogon::HttpStatusCode>(fsError->GetStatus()));
       callback(resp);
       return;
     }
-    // TODO debug logger
-
     status = process_responses(respBuffs, dbResponseIntf);
-    // TODO debug logger
     if (status.err_file_name[0] != '\0') {
       auto fsError = TranslateRonDbError(status.http_code, status.message);
       resp->setBody(fsError->Error());
-      resp->setStatusCode(static_cast<drogon::HttpStatusCode>(fsError->GetStatus()));
+      resp->setStatusCode(
+        static_cast<drogon::HttpStatusCode>(fsError->GetStatus()));
       callback(resp);
       return;
     }
-
     for (unsigned long i = 0; i < noOps; i++) {
       rsBufferArrayManager.return_resp_buffer(respBuffs[i]);
       rsBufferArrayManager.return_req_buffer(reqBuffs[i]);
     }
-
     std::tie(features, err) = getFeatureValuesMultipleEntries(
-        dbResponseIntf, reqStruct.entries, *metadata, featureStatus, detailedStatus,
+        dbResponseIntf,
+        reqStruct.entries,
+        *metadata,
+        featureStatus,
+        detailedStatus,
         reqStruct.GetOptions().includeDetailedStatus);
     if (err != nullptr) {
       resp->setBody(err->Error());
@@ -218,16 +234,18 @@ void BatchFeatureStoreCtrl::batch_featureStore(
       return;
     }
   } else {
-    auto emptyFeatures = std::vector<std::vector<std::vector<char>>>(reqStruct.entries.size());
-    features           = emptyFeatures;
+    auto emptyFeatures =
+      std::vector<std::vector<std::vector<char>>>(reqStruct.entries.size());
+    features = emptyFeatures;
   }
   fsResp.status = featureStatus;
-  fillPassedFeaturesMultipleEntries(features, reqStruct.passedFeatures,
-                                    metadata->prefixFeaturesLookup, metadata->featureIndexLookup,
+  fillPassedFeaturesMultipleEntries(features,
+                                    reqStruct.passedFeatures,
+                                    metadata->prefixFeaturesLookup,
+                                    metadata->featureIndexLookup,
                                     featureStatus);
   fsResp.features = features;
   fsResp.metadata = GetFeatureMetadata(metadata, reqStruct.metadataRequest);
-
   resp->setContentTypeCode(drogon::CT_APPLICATION_JSON);
   auto fsRespStr = fsResp.to_string();
   resp->setBody(fsRespStr);
@@ -235,20 +253,23 @@ void BatchFeatureStoreCtrl::batch_featureStore(
   callback(resp);
 }
 
-unsigned checkFeatureStatus(const feature_store_data_structs::BatchFeatureStoreRequest &fsReq,
-                            const metadata::FeatureViewMetadata &metadata,
-                            std::vector<feature_store_data_structs::FeatureStatus> &status) {
+unsigned checkFeatureStatus(
+  const feature_store_data_structs::BatchFeatureStoreRequest &fsReq,
+  const metadata::FeatureViewMetadata &metadata,
+  std::vector<feature_store_data_structs::FeatureStatus> &status) {
   auto cnt = std::unordered_map<unsigned, bool>();
   for (unsigned i = 0; i < fsReq.entries.size(); ++i) {
-    if (ValidatePrimaryKey(fsReq.entries[i], metadata.validPrimayKeys) != nullptr) {
+    if (ValidatePrimaryKey(fsReq.entries[i],
+                           metadata.validPrimayKeys) != nullptr) {
       status[i] = feature_store_data_structs::FeatureStatus::Error;
       cnt[i]    = true;
     }
   }
-
-  if (!fsReq.passedFeatures.empty() && fsReq.GetOptions().validatePassedFeatures) {
+  if (!fsReq.passedFeatures.empty() &&
+      fsReq.GetOptions().validatePassedFeatures) {
     for (unsigned i = 0; i < fsReq.passedFeatures.size(); ++i) {
-      if (ValidatePassedFeatures(fsReq.passedFeatures[i], metadata.prefixFeaturesLookup) !=
+      if (ValidatePassedFeatures(fsReq.passedFeatures[i],
+                                 metadata.prefixFeaturesLookup) !=
           nullptr) {
         status[i] = feature_store_data_structs::FeatureStatus::Error;
         cnt[i]    = true;
@@ -258,31 +279,35 @@ unsigned checkFeatureStatus(const feature_store_data_structs::BatchFeatureStoreR
   return status.size() - cnt.size();
 }
 
-std::tuple<std::vector<std::vector<std::vector<char>>>, std::shared_ptr<RestErrorCode>>
+std::tuple<std::vector<std::vector<std::vector<char>>>,
+                       std::shared_ptr<RestErrorCode>>
 getFeatureValuesMultipleEntries(
-    BatchResponseJSON &batchResponse,
-    const std::vector<std::unordered_map<std::string, std::vector<char>>> &entries,
-    const metadata::FeatureViewMetadata &featureView,
-    std::vector<feature_store_data_structs::FeatureStatus> &batchStatus,
-    std::vector<std::vector<feature_store_data_structs::DetailedStatus>> &detailedStatus,
-    bool includeDetailedStatus) {
-  auto rondbBatchResult = std::vector<std::vector<PKReadResponseWithCodeJSON>>(batchStatus.size());
-  auto batchResult      = std::vector<std::vector<std::vector<char>>>(batchStatus.size());
-
+  BatchResponseJSON &batchResponse,
+  const std::vector<std::unordered_map<std::string,
+  std::vector<char>>> &entries,
+  const metadata::FeatureViewMetadata &featureView,
+  std::vector<feature_store_data_structs::FeatureStatus> &batchStatus,
+  std::vector<std::vector<
+    feature_store_data_structs::DetailedStatus>> &detailedStatus,
+  bool includeDetailedStatus) {
+  auto rondbBatchResult =
+    std::vector<std::vector<PKReadResponseWithCodeJSON>>(batchStatus.size());
+  auto batchResult =
+    std::vector<std::vector<std::vector<char>>>(batchStatus.size());
   for (auto &response : batchResponse.getResult()) {
-    auto operationId            = response.getBody().getOperationID();
-    auto separatorPos           = operationId.find(SEQUENCE_SEPARATOR);
-    auto splitOperationIdFirst  = operationId.substr(0, separatorPos);
+    auto operationId = response.getBody().getOperationID();
+    auto separatorPos = operationId.find(SEQUENCE_SEPARATOR);
+    auto splitOperationIdFirst = operationId.substr(0, separatorPos);
     auto splitOperationIdSecond = operationId.substr(separatorPos + 1);
-
     int seqNum = 0;
     try {
       seqNum = std::stoi(splitOperationIdFirst);
     } catch (...) {
       return std::make_tuple(std::vector<std::vector<std::vector<char>>>(),
                              std::make_shared<RestErrorCode>(
-                                 "Failed to parse sequence number.",
-                                 static_cast<int>(drogon::k500InternalServerError)));
+                               "Failed to parse sequence number.",
+                               static_cast<int>(
+                                 drogon::k500InternalServerError)));
     }
     response.setOperationId(splitOperationIdSecond);
     rondbBatchResult[seqNum].push_back(response);
@@ -291,7 +316,10 @@ getFeatureValuesMultipleEntries(
   for (unsigned i = 0; i < rondbBatchResult.size(); ++i) {
     if (!rondbBatchResult[i].empty()) {
       auto result =
-          GetFeatureValues(rondbBatchResult[i], entries[i], featureView, includeDetailedStatus);
+          GetFeatureValues(rondbBatchResult[i],
+                           entries[i],
+                           featureView,
+                           includeDetailedStatus);
       batchResult[i] = std::get<0>(result);
       batchStatus[i] = std::get<1>(result);
       if (includeDetailedStatus) {
@@ -304,14 +332,15 @@ getFeatureValuesMultipleEntries(
 
 std::vector<PKReadParams> getBatchPkReadParamsMultipleEntries(
     const metadata::FeatureViewMetadata &metadata,
-    const std::vector<std::unordered_map<std::string, std::vector<char>>> &entries,
+    const std::vector<std::unordered_map<std::string,
+    std::vector<char>>> &entries,
     std::vector<feature_store_data_structs::FeatureStatus> &status) {
   auto batchReadParams = std::vector<PKReadParams>();
   batchReadParams.reserve(metadata.numOfFeatures * entries.size());
   for (unsigned i = 0; i < entries.size(); ++i) {
     if (status[i] != feature_store_data_structs::FeatureStatus::Error) {
       for (auto &param : GetBatchPkReadParams(metadata, entries[i])) {
-        auto oid          = std::to_string(i) + SEQUENCE_SEPARATOR + param.operationId;
+        auto oid = std::to_string(i) + SEQUENCE_SEPARATOR + param.operationId;
         param.operationId = oid;
         batchReadParams.push_back(param);
       }
@@ -320,8 +349,9 @@ std::vector<PKReadParams> getBatchPkReadParamsMultipleEntries(
   return batchReadParams;
 }
 
-BatchResponseJSON getPkReadResponseJson(int numEntries,
-                                        const metadata::FeatureViewMetadata &metadata) {
+BatchResponseJSON
+  getPkReadResponseJson(int numEntries,
+                        const metadata::FeatureViewMetadata &metadata) {
   auto response = BatchResponseJSON();
   response.Init(metadata.featureGroupFeatures.size() * numEntries);
   return response;
@@ -329,14 +359,19 @@ BatchResponseJSON getPkReadResponseJson(int numEntries,
 
 void fillPassedFeaturesMultipleEntries(
     std::vector<std::vector<std::vector<char>>> &features,
-    const std::vector<std::unordered_map<std::string, std::vector<char>>> &passedFeatures,
-    std::unordered_map<std::string, std::vector<metadata::FeatureMetadata>> &featureMetadata,
+    const std::vector<std::unordered_map<
+      std::string, std::vector<char>>> &passedFeatures,
+    std::unordered_map<std::string,
+      std::vector<metadata::FeatureMetadata>> &featureMetadata,
     std::unordered_map<std::string, int> &indexLookup,
     const std::vector<feature_store_data_structs::FeatureStatus> &status) {
   if (!passedFeatures.empty() && !passedFeatures.empty()) {
     for (unsigned i = 0; i < features.size(); ++i) {
       if (status[i] != feature_store_data_structs::FeatureStatus::Error) {
-        FillPassedFeatures(features[i], passedFeatures[i], featureMetadata, indexLookup);
+        FillPassedFeatures(features[i],
+                           passedFeatures[i],
+                           featureMetadata,
+                           indexLookup);
       }
     }
   }
