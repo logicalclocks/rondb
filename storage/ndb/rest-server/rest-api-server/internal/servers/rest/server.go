0a796222d0d850c7d0c2fe6a8dcd46ae616af6ca
Salman Niazi <salman@logicalclocks.com>
2025-03-11 13:30:31 +0100

    fixed mem issues for batch feature store

--- storage/ndb/rest-server/rest-api-server/internal/servers/rest/server.go
+++ storage/ndb/rest-server/rest-api-server/internal/servers/rest/server.go
@@ -128,7 +128,7 @@ func registerHandlers(router *gin.Engine, heap *heap.Heap, apiKeyCache apikey.Ca
 		batchPkReadHandler:       batchPkReadHandler,
 		rdrsMetrics:              rdrsMetrics,
 		featureStoreHandler:      featureStoreHandler,
-		batchFeatureStoreHandler: batchfeaturestore.New(fvMeta, apiKeyCache, batchPkReadHandler),
+		batchFeatureStoreHandler: batchfeaturestore.New(fvMeta, apiKeyCache, batchPkReadHandler, heap),
 	}
 
 	// ping
