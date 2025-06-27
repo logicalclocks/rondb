786ea427f53e12e9fcde375d09b330a363782ff2
Mikael Ronström <mikael.ronstrom@gmail.com>
2025-06-27 15:28:47 +0200

    RONDB-920: Review fixes for upgrade handling

--- storage/ndb/plugin/ndb_dd_sdi.cc
+++ storage/ndb/plugin/ndb_dd_sdi.cc
@@ -101,16 +101,14 @@ static bool check_sdi_compatibility(const dd::RJ_Document &doc) {
   assert(doc.HasMember("mysqld_version_id"));
   const dd::RJ_Value &mysqld_version_id = doc["mysqld_version_id"];
   assert(mysqld_version_id.IsUint64());
-  if ((mysqld_version_id.GetUint64() > std::uint64(221100) &&
-       mysqld_version_id.GetUint64() < std::uint64(241000)) ||
-      mysqld_version_id.GetUint64() >= std::uint64(241100) ||
-      mysqld_version_id.GetUint64() < std::uint64(221003)) {
+  if ((mysqld_version_id.GetUint64() > std::uint64(221100)) {
     /**
-     * We cannot handle deserialize from version before 22.10.3
-     * and also not for versions after 24.10 series. We can handle
-     * deserialisation from 22.10.3 and newer versions in the 22.10
-     * series, we can also handle all 24.10 versions. These should
-     * using a compatible serialisation of the SDI.
+     * We cannot handle deserialize from version newer than 22.10,
+     * thus we cannot handle tables created in e.g. 24.10.x.
+     * We can handle tables created in earlier versions always.
+     *
+     * Since this code is only used in 22.10.13 and newer we need
+     * not check any more.
      */
     my_error(ER_IMP_INCOMPATIBLE_MYSQLD_VERSION, MYF(0),
              mysqld_version_id.GetUint64(), std::uint64_t(MYSQL_VERSION_ID));
