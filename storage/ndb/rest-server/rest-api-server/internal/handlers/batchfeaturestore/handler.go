0ea46a22f7bf01db79216112c4c0c4752cb8971e
Salman Niazi <salman@logicalclocks.com>
2025-10-23 13:16:16 +0200

    RONDB-968: Prevents Go's GC from seeing dangling pointers to freed C memory by clearing the references before the memory is freed.

--- storage/ndb/rest-server/rest-api-server/internal/handlers/batchfeaturestore/handler.go
+++ storage/ndb/rest-server/rest-api-server/internal/handlers/batchfeaturestore/handler.go
@@ -171,6 +171,12 @@ func (h *Handler) Execute(request interface{}, response interface{}) (int, func(
 		// --- buffers ---
 
 		code, ronDbErr := h.dbBatchHandler.ExecuteWithBuffers(readParams, *dbResponseIntf, reqPtrs, respPtrs, noOps)
+
+		// Clear Go references to prevent use-after-free when release() calls C.free()
+		// The NativeBuffer structs contain unsafe.Pointer to C memory that will be freed
+		reqPtrs = nil
+		respPtrs = nil
+
 		if log.IsDebug() {
 			log.Debugf("RonDB response: code: %d, error: %s, body: %s", code, ronDbErr, (*dbResponseIntf).String())
 		}
