b7d3b4a4c667f196bb135985ef5b2744c93e75fe
Mikael Ronström <mikael.ronstrom@gmail.com>
2026-01-06 14:10:51 +0100

    RONDB-978: Ensure TCKEYREQ with key length not use user id flag

--- storage/ndb/src/kernel/blocks/dbdict/Dbdict.cpp
+++ storage/ndb/src/kernel/blocks/dbdict/Dbdict.cpp
@@ -28237,6 +28237,7 @@ void Dbdict::execSCHEMA_TRANS_IMPL_REF(Signal *signal) {
 
   if (ref->errorCode) {
     jam();
+    ndbrequire(ref->errorCode != 626);
     ErrorInfo error;
     setError(error, ref);
     setError(trans_ptr, error);
@@ -30777,6 +30778,7 @@ void Dbdict::sendTransClientReply(Signal *signal, SchemaTransPtr trans_ptr) {
       ref->masterNodeId = c_masterNodeId;
       sendSignal(receiverRef, GSN_SCHEMA_TRANS_END_REF, signal,
                  SchemaTransEndRef::SignalLength, JBB);
+      ndbrequire(ref->errorCode != 626);
     }
     resetError(trans_ptr);
     trans_ptr.p->m_clientState = TransClient::EndReply;
