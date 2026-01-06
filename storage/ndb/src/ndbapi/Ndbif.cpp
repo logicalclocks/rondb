1cb685bab23d768e867c85982e4131e812672256
Mikael Ronström <mikael.ronstrom@gmail.com>
2026-01-06 15:13:33 +0100

    RONDB-978: Missing break

--- storage/ndb/src/ndbapi/Ndbif.cpp
+++ storage/ndb/src/ndbapi/Ndbif.cpp
@@ -893,6 +893,7 @@ void NdbImpl::trp_deliver_signal(const NdbApiSignal *aSignal,
       tReturnCode = tCon->receiveGET_DATABASE_REF(aSignal);
       if (tReturnCode != -1) {
         tNewState = NO_WAIT;
+        break;
       } else {
         goto InvalidSignal;
       }
