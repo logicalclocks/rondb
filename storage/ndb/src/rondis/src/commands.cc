29ccd95d45fafe18e401350b9e231dc6ca8c6c16
mikael <mikael@logicalclocks.com>
2026-05-12 15:29:03 +0200

    RONDB-1053: Add a few asserts to clarify the code

--- storage/ndb/src/rondis/src/commands.cc
+++ storage/ndb/src/rondis/src/commands.cc
@@ -2940,6 +2940,8 @@ void rondb_mset(Ndb *ndb,
     hset_done = true;
   }
   if (get_cmd_part) {
+    assert(!is_hash_command);
+    assert(num_keys == 1);
     int ret_code = rondb_get_func(ndb,
                                   tab,
                                   response,
@@ -2959,6 +2961,7 @@ void rondb_mset(Ndb *ndb,
   }
   DEB_MSET_CMD(("MSET of %u keys\n", num_keys));
   if (!hset_done) {
+    assert(!is_hash_command);
     Uint32 current_index = 0;
     do {
       Uint32 loop_count = std::min(num_keys - current_index,
